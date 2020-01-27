78eaf1af6db06c1a82332e862db2ac43342a7af3
chokniran chongsomchit <chokniran@gmail.com>
2020-01-27 14:31:53 +0700

    add object login form

--- frontend/src/app/components/member/login/login.component.ts
+++ frontend/src/app/components/member/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,15 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  myApp = "POS";
-  
+  myApp = 'POS';
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  // any (default)
+  login(LoginForm:NgForm){
+      alert(JSON.stringify(LoginForm.value));
+  }
+
   onSayHi(){
-     alert(this.myApp + this.onGetName())
+     //alert(this.myApp + this.onGetName())
   }
 
   onGetName(): String{
