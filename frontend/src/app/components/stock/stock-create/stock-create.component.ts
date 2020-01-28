2f05b707b79d4a631ca2599abbb1ab84302b9f8a
chokniran chongsomchit <chokniran@gmail.com>
2020-01-28 15:49:54 +0700

    prepair stock create

--- frontend/src/app/components/stock/stock-create/stock-create.component.ts
+++ frontend/src/app/components/stock/stock-create/stock-create.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from 'src/app/models/product.model';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-stock-create',
@@ -6,10 +8,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./stock-create.component.css']
 })
 export class StockCreateComponent implements OnInit {
-
-  constructor() { }
+  mProduct = new Product;
+  imageSrc: string | ArrayBuffer;
+  constructor(private location: Location) {
+     this.mProduct.name  = "";
+     this.mProduct.price = 0;
+     this.mProduct.stock = 0;
+   }
 
   ngOnInit() {
+  }
+
+  onSubmit(){
+    alert(this.mProduct.name);
+  }
+
+  onCancel(){
+    this.location.back();
+  }
+
+  onUpload(event) {
+    const metaImage = event.target.files[0];
+    if (metaImage) {
+      const reader = new FileReader();
+      reader.readAsDataURL(metaImage);
+      reader.onload = () => {
+        this.imageSrc = reader.result;
+        this.mProduct.image = metaImage;
+      };
+    }
   }
 
 }
