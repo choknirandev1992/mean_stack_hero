9d2ba4cc09944f4bd0d7c4d03f844a1d796f29b8
chokniran chongsomchit <chokniran@gmail.com>
2020-01-28 14:45:12 +0700

    prepair stock home

--- frontend/src/app/components/stock/stock-home/stock-home.component.ts
+++ frontend/src/app/components/stock/stock-home/stock-home.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Subject } from 'rxjs';
+import { debounceTime } from "rxjs/operators";
 
 @Component({
   selector: 'app-stock-home',
@@ -6,10 +9,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./stock-home.component.css']
 })
 export class StockHomeComponent implements OnInit {
+  mDataArray = [11,22,33,44,55];
+  mTextSearch = new Subject<string>();
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
+     this.mTextSearch.pipe(
+       debounceTime(2000)
+     ).subscribe(keyword => this.search(keyword));
+  }
+
+  search(keyword: string): void {
+    console.log(keyword);
+  }
+
+  outofStock(){
+     return 1150;
+  }
+
+  onClickDelete(id: string){
+     //modal confirm
+  }
+
+  onClickEdit(id: string){
+     this.router.navigate([`/stock/edit/${id}`])
   }
 
 }
